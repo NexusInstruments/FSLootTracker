048f98a8a144457fcd45754aa26863d210da9419
Jay Reardon <jay.reardon@lonelydaystar.com>
2014-07-16 03:18:18 -0700

    Added Decode functionality back into the project

--- JsonUtils-1.0.lua
+++ JsonUtils-1.0.lua
@@ -14,9 +14,12 @@ JsonUtils.embeded = JsonUtils.embeded or {}
 
 -- upgrading of embeded is done at the bottom of the file
 local mixins = {
-	"PrintProps", "onEncodeError", 
-	"Encode", "EncodePretty"
+	"PrintProps", "onDecodeError", "onEncodeError", 
+	"JSONDecode", "JSONEncode", "JSONEncodePretty"
 }   
+
+local isArray  = { __tostring = function() return "JSON array"  end }    isArray.__index  = isArray
+local isObject = { __tostring = function() return "JSON object" end }    isObject.__index = isObject
 
 local chars_to_be_escaped_in_JSON_string
    = '['
@@ -26,6 +29,9 @@ local chars_to_be_escaped_in_JSON_string
    ..    '\001' .. '-' .. '\031' -- class sub-pattern to match control characters
    .. ']'
 
+-----------------------------------------------------------------------------------------------
+-- JsonUtils Testing Functions
+-----------------------------------------------------------------------------------------------   
 local function object_or_array(self, T, etc)
    --
    -- We need to inspect all the keys... if there are any strings, we'll convert to a JSON
@@ -110,6 +116,72 @@ local function object_or_array(self, T, etc)
    return string_keys, nil, map
 end
 
+-----------------------------------------------------------------------------------------------
+-- JsonUtils String Manipulation Function
+-----------------------------------------------------------------------------------------------
+local function unicode_codepoint_as_utf8(codepoint)
+   --
+   -- codepoint is a number
+   --
+   if codepoint <= 127 then
+      return string.char(codepoint)
+
+   elseif codepoint <= 2047 then
+      --
+      -- 110yyyxx 10xxxxxx         <-- useful notation from http://en.wikipedia.org/wiki/Utf8
+      --
+      local highpart = math.floor(codepoint / 0x40)
+      local lowpart  = codepoint - (0x40 * highpart)
+      return string.char(0xC0 + highpart,
+                         0x80 + lowpart)
+
+   elseif codepoint <= 65535 then
+      --
+      -- 1110yyyy 10yyyyxx 10xxxxxx
+      --
+      local highpart  = math.floor(codepoint / 0x1000)
+      local remainder = codepoint - 0x1000 * highpart
+      local midpart   = math.floor(remainder / 0x40)
+      local lowpart   = remainder - 0x40 * midpart
+
+      highpart = 0xE0 + highpart
+      midpart  = 0x80 + midpart
+      lowpart  = 0x80 + lowpart
+
+      --
+      -- Check for an invalid character (thanks Andy R. at Adobe).
+      -- See table 3.7, page 93, in http://www.unicode.org/versions/Unicode5.2.0/ch03.pdf#G28070
+      --
+      if ( highpart == 0xE0 and midpart < 0xA0 ) or
+         ( highpart == 0xED and midpart > 0x9F ) or
+         ( highpart == 0xF0 and midpart < 0x90 ) or
+         ( highpart == 0xF4 and midpart > 0x8F )
+      then
+         return "?"
+      else
+         return string.char(highpart,
+                            midpart,
+                            lowpart)
+      end
+
+   else
+      --
+      -- 11110zzz 10zzyyyy 10yyyyxx 10xxxxxx
+      --
+      local highpart  = math.floor(codepoint / 0x40000)
+      local remainder = codepoint - 0x40000 * highpart
+      local midA      = math.floor(remainder / 0x1000)
+      remainder       = remainder - 0x1000 * midA
+      local midB      = math.floor(remainder / 0x40)
+      local lowpart   = remainder - 0x40 * midB
+
+      return string.char(0xF0 + highpart,
+                         0x80 + midA,
+                         0x80 + midB,
+                         0x80 + lowpart)
+   end
+end
+
 local function backslash_replacement_function(c)
    if c == "\n" then
       return "\\n"
@@ -130,41 +202,256 @@ local function backslash_replacement_function(c)
    end
 end
     
-function JsonUtils:PrintProps(o, b)
-	local strBase = ""
-	if b ~= nil then 
-		strBase = b 
-	end
-	
-	if type(o) == table then
-		for key,value in pairs(o) do
-			Print(strBase .. "." .. key .. ":" .. printProp(value, key));
-		end
-	else
-		return o
-	end 
-end
-
-function JsonUtils:onEncodeError(message, etc)
-   if etc ~= nil then
-      message = message .. " (" .. JsonUtils:encode(etc) .. ")"
-   end
-
-   if self.assert then
-      self.assert(false, message)
-   else
-      Print(message)
-   end
-end
-
 local function json_string_literal(value)
    local newval = string.gsub(value, chars_to_be_escaped_in_JSON_string, backslash_replacement_function)
    return '"' .. newval .. '"'
 end
+	
+-----------------------------------------------------------------------------------------------
+-- JsonUtils string parser functions
+-----------------------------------------------------------------------------------------------
+local function skip_whitespace(text, start)
 
---
--- Encode
---
+   local match_start, match_end = text:find("^[ \n\r\t]+", start) -- [http://www.ietf.org/rfc/rfc4627.txt] Section 2
+   if match_end then
+      return match_end + 1
+   else
+      return start
+   end
+end
+
+-- Parses a Number	
+local function grok_number(self, text, start, etc)
+   --
+   -- Grab the integer part
+   --
+   local integer_part = text:match('^-?[1-9]%d*', start)
+                     or text:match("^-?0",        start)
+
+   if not integer_part then
+      self:onDecodeError("expected number", text, start, etc)
+   end
+
+   local i = start + integer_part:len()
+
+   --
+   -- Grab an optional decimal part
+   --
+   local decimal_part = text:match('^%.%d+', i) or ""
+
+   i = i + decimal_part:len()
+
+   --
+   -- Grab an optional exponential part
+   --
+   local exponent_part = text:match('^[eE][-+]?%d+', i) or ""
+
+   i = i + exponent_part:len()
+
+   local full_number_text = integer_part .. decimal_part .. exponent_part
+   local as_number = tonumber(full_number_text)
+
+   if not as_number then
+      self:onDecodeError("bad number", text, start, etc)
+   end
+
+   return as_number, i
+end
+
+-- Parses a String
+local function grok_string(self, text, start, etc)
+
+   if text:sub(start,start) ~= '"' then
+      self:onDecodeError("expected string's opening quote", text, start, etc)
+   end
+
+   local i = start + 1 -- +1 to bypass the initial quote
+   local text_len = text:len()
+   local VALUE = ""
+   while i <= text_len do
+      local c = text:sub(i,i)
+      if c == '"' then
+         return VALUE, i + 1
+      end
+      if c ~= '\\' then
+         VALUE = VALUE .. c
+         i = i + 1
+      elseif text:match('^\\b', i) then
+         VALUE = VALUE .. "\b"
+         i = i + 2
+      elseif text:match('^\\f', i) then
+         VALUE = VALUE .. "\f"
+         i = i + 2
+      elseif text:match('^\\n', i) then
+         VALUE = VALUE .. "\n"
+         i = i + 2
+      elseif text:match('^\\r', i) then
+         VALUE = VALUE .. "\r"
+         i = i + 2
+      elseif text:match('^\\t', i) then
+         VALUE = VALUE .. "\t"
+         i = i + 2
+      else
+         local hex = text:match('^\\u([0123456789aAbBcCdDeEfF][0123456789aAbBcCdDeEfF][0123456789aAbBcCdDeEfF][0123456789aAbBcCdDeEfF])', i)
+         if hex then
+            i = i + 6 -- bypass what we just read
+
+            -- We have a Unicode codepoint. It could be standalone, or if in the proper range and
+            -- followed by another in a specific range, it'll be a two-code surrogate pair.
+            local codepoint = tonumber(hex, 16)
+            if codepoint >= 0xD800 and codepoint <= 0xDBFF then
+               -- it's a hi surrogate... see whether we have a following low
+               local lo_surrogate = text:match('^\\u([dD][cdefCDEF][0123456789aAbBcCdDeEfF][0123456789aAbBcCdDeEfF])', i)
+               if lo_surrogate then
+                  i = i + 6 -- bypass the low surrogate we just read
+                  codepoint = 0x2400 + (codepoint - 0xD800) * 0x400 + tonumber(lo_surrogate, 16)
+               else
+                  -- not a proper low, so we'll just leave the first codepoint as is and spit it out.
+               end
+            end
+            VALUE = VALUE .. unicode_codepoint_as_utf8(codepoint)
+
+         else
+
+            -- just pass through what's escaped
+            VALUE = VALUE .. text:match('^\\(.)', i)
+            i = i + 2
+         end
+      end
+   end
+
+   self:onDecodeError("unclosed string", text, start, etc)
+end
+
+local grok_one -- assigned later
+
+-- Parses an Object
+local function grok_object(self, text, start, etc)
+   if not text:sub(start,start) == '{' then
+      self:onDecodeError("expected '{'", text, start, etc)
+   end
+
+   local i = skip_whitespace(text, start + 1) -- +1 to skip the '{'
+
+   local VALUE = self.strictTypes and self:newObject { } or { }
+
+   if text:sub(i,i) == '}' then
+      return VALUE, i + 1
+   end
+   local text_len = text:len()
+   while i <= text_len do
+      local key, new_i = grok_string(self, text, i, etc)
+
+      i = skip_whitespace(text, new_i)
+
+      if text:sub(i, i) ~= ':' then
+         self:onDecodeError("expected colon", text, i, etc)
+      end
+
+      i = skip_whitespace(text, i + 1)
+
+      local val, new_i = grok_one(self, text, i)
+
+      VALUE[key] = val
+
+      --
+      -- Expect now either '}' to end things, or a ',' to allow us to continue.
+      --
+      i = skip_whitespace(text, new_i)
+
+      local c = text:sub(i,i)
+
+      if c == '}' then
+         return VALUE, i + 1
+      end
+
+      if text:sub(i, i) ~= ',' then
+         self:onDecodeError("expected comma or '}'", text, i, etc)
+      end
+
+      i = skip_whitespace(text, i + 1)
+   end
+
+   self:onDecodeError("unclosed '{'", text, start, etc)
+end
+
+-- Parses an Array
+local function grok_array(self, text, start, etc)
+   if not text:sub(start,start) == '[' then
+      self:onDecodeError("expected '['", text, start, etc)
+   end
+
+   local i = skip_whitespace(text, start + 1) -- +1 to skip the '['
+   local VALUE = self.strictTypes and self:newArray { } or { }
+   if text:sub(i,i) == ']' then
+      return VALUE, i + 1
+   end
+
+   local VALUE_INDEX = 1
+
+   local text_len = text:len()
+   while i <= text_len do
+      local val, new_i = grok_one(self, text, i)
+
+      -- can't table.insert(VALUE, val) here because it's a no-op if val is nil
+      VALUE[VALUE_INDEX] = val
+      VALUE_INDEX = VALUE_INDEX + 1
+
+      i = skip_whitespace(text, new_i)
+
+      --
+      -- Expect now either ']' to end things, or a ',' to allow us to continue.
+      --
+      local c = text:sub(i,i)
+      if c == ']' then
+         return VALUE, i + 1
+      end
+      if text:sub(i, i) ~= ',' then
+         self:onDecodeError("expected comma or '['", text, i, etc)
+      end
+      i = skip_whitespace(text, i + 1)
+   end
+   self:onDecodeError("unclosed '['", text, start, etc)
+end
+
+-- Parses the next element in the string
+grok_one = function(self, text, start, etc)
+   -- Skip any whitespace
+   start = skip_whitespace(text, start)
+
+   if start > text:len() then
+      self:onDecodeError("unexpected end of string", text, nil, etc)
+   end
+
+   if text:find('^"', start) then
+      return grok_string(self, text, start, etc)
+
+   elseif text:find('^[-0123456789 ]', start) then
+      return grok_number(self, text, start, etc)
+
+   elseif text:find('^%{', start) then
+      return grok_object(self, text, start, etc)
+
+   elseif text:find('^%[', start) then
+      return grok_array(self, text, start, etc)
+
+   elseif text:find('^true', start) then
+      return true, start + 4
+
+   elseif text:find('^false', start) then
+      return false, start + 5
+
+   elseif text:find('^null', start) then
+      return nil, start + 4
+
+   else
+      self:onDecodeError("can't parse JSON", text, start, etc)
+   end
+end	
+
+-----------------------------------------------------------------------------------------------
+-- JsonUtils Encode Value
+-----------------------------------------------------------------------------------------------	
 local encode_value
 local function encode_value(self, value, parents, etc, indent) -- non-nil indent means pretty-printing
    local val = value
@@ -298,20 +585,119 @@ local function encode_value(self, value, parents, etc, indent) -- non-nil indent
    end
 end
 
-function JsonUtils:Encode(value, etc)
-   --if type(self) ~= 'table' or self.__index ~= JsonUtils then
-   --   JsonUtils:onEncodeError("JSON:encode must be called in method format", etc)
+-----------------------------------------------------------------------------------------------
+-- JsonUtils Decode Value
+-----------------------------------------------------------------------------------------------
+function decode(text, etc)
+   if text == nil then
+      self:onDecodeOfNilError(string.format("nil passed to JsonUtils:decode()"), nil, nil, etc)
+   elseif type(text) ~= 'string' then
+      self:onDecodeError(string.format("expected string argument to JsonUtils:decode(), got %s", type(text)), nil, nil, etc)
+   end
+
+   if text:match('^%s*$') then
+      return nil
+   end
+
+   --
+   -- Ensure that it's not UTF-32 or UTF-16.
+   -- Those are perfectly valid encodings for JSON (as per RFC 4627 section 3),
+   -- but this package can't handle them.
+   --
+   --if text:sub(1,1):byte() == 0 or (text:len() >= 2 and text:sub(2,2):byte() == 0) then
+   --  self:onDecodeError("JSON package groks only UTF-8, sorry", text, nil, etc)
    --end
+
+   local success, value = pcall(grok_one, self, text, 1, etc)
+
+   if success then
+      return value
+   else
+      -- if JSON:onDecodeError() didn't abort out of the pcall, we'll have received the error message here as "value", so pass it along as an assert.
+      Print(value)
+      -- and if we're still here, return a nil and throw the error message on as a second arg
+      return nil, value
+   end
+end
+
+-----------------------------------------------------------------------------------------------
+-- JsonUtils Utility Functions
+-----------------------------------------------------------------------------------------------
+function JsonUtils.__tostring()
+   return "JSON encode/decode package"
+end
+
+function JsonUtils:newArray(tbl)
+   return setmetatable(tbl or {}, isArray)
+end
+
+function JsonUtils:newObject(tbl)
+   return setmetatable(tbl or {}, isObject)
+end
+
+function JsonUtils:PrintProps(o, b)
+	local strBase = ""
+	if b ~= nil then 
+		strBase = b 
+	end
+	
+	if type(o) == table then
+		for key,value in pairs(o) do
+			Print(strBase .. "." .. key .. ":" .. printProp(value, key));
+		end
+	else
+		return o
+	end 
+end
+
+-----------------------------------------------------------------------------------------------
+-- JsonUtils Error Handling
+-----------------------------------------------------------------------------------------------
+function JsonUtils:onDecodeError(message, text, location, etc)
+   if text then
+      if location then
+         message = string.format("%s at char %d of: %s", message, location, text)
+      else
+         message = string.format("%s: %s", message, text)
+      end
+   end
+
+   if etc ~= nil then
+      message = message .. " (" .. JsonUtils:JSONEncode(etc) .. ")"
+   end
+
+   Print(message)
+end
+
+JsonUtils.onDecodeOfNilError  = JsonUtils.onDecodeError
+JsonUtils.onDecodeOfHTMLError = JsonUtils.onDecodeError
+
+function JsonUtils:onEncodeError(message, etc)
+   if etc ~= nil then
+      message = message .. " (" .. JsonUtils:JSONEncode(etc) .. ")"
+   end
+
+   Print(message)
+end
+
+-----------------------------------------------------------------------------------------------
+-- JsonUtils Public Methods
+-----------------------------------------------------------------------------------------------
+function JsonUtils:JSONDecode(value, etc)
+   return decode(value, etc)
+end
+
+function JsonUtils:JSONEncode(value, etc)
    return encode_value(self, value, {}, etc, nil)
 end
 
-function JsonUtils:EncodePretty(value, etc)
-   --if type(self) ~= 'table' or self.__index ~= JsonUtils then
-   --   JsonUtils:onEncodeError("JSON:encode_pretty must be called in method format", etc)
-   --end
+function JsonUtils:JSONEncodePretty(value, etc)
    return encode_value(self, value, {}, etc, "")
 end
 
+-----------------------------------------------------------------------------------------------
+-- JsonUtils Constructor
+-----------------------------------------------------------------------------------------------
 function JsonUtils:new(args)
    local new = { }
 
@@ -324,6 +710,9 @@ function JsonUtils:new(args)
    return setmetatable(new, JsonUtils)
 end
 
+-----------------------------------------------------------------------------------------------
+-- JsonUtils Library / Apollo Routines
+-----------------------------------------------------------------------------------------------
 function JsonUtils:Embed( target )
 	for k, v in pairs( mixins ) do
 		target[v] = self[v]
@@ -332,14 +721,22 @@ function JsonUtils:Embed( target )
 	return target
 end
 
+-- No special on Init code
+function JsonUtils:OnLoad() 
+end
+
+-- No dependencies
+function JsonUtils:OnDependencyError(strDep, strError) return false 
+end
+
+
+-----------------------------------------------------------------------------------------------
+-- JsonUtils Instantiation/ Registration
+-----------------------------------------------------------------------------------------------
+
 --- Upgrade our old embeded
 for target, v in pairs( JsonUtils.embeded ) do
 	JsonUtils:Embed( target )
 end
-
--- No special on Init code
-function JsonUtils:OnLoad() end
--- No dependencies
-function JsonUtils:OnDependencyError(strDep, strError) return false end
 
 Apollo.RegisterPackage(JsonUtils, JSUMAJOR, JSUMINOR, {})
--- README.md
+++ README.md
@@ -1,14 +1,47 @@
 JsonUtils
 =========
-
 A Wildstar Embeded LUA library to Encode LUA objects into JSON strings
 
-This Addon is a partial port of Jeffery Friedl's JSON Lua library.
+This can be used to export data or to transport data communications over in-game comm-channels.
 
+This Addon is a partial port of Jeffery Friedl's JSON Lua library.
 See licensing information at the end.
 
 Usage
 =====
+
+Embedding
+---------
+```lua
+  Apollo.GetPackage("Json:Utils-1.0").tPackage:Embed(self)
+
+  self:JSONEncode({ a = 1 })
+```
+
+JSONDecode
+----------
+''Example''
+```lua
+  Apollo.GetPackage("Json:Utils-1.0").tPackage:Embed(self)
+  
+  local jsonStr = "[{"a":1,"b":"string","c":{"a":1,"b":"string"},"d":null}]"
+
+  local tNewTable = self:JSONEncode(jsonStr)
+```
+Results
+```
+Value of tNewTable:
+{
+    a = 1, 
+    b = "string", 
+    c = { a = 1, b = "string" }
+    d = nil
+}
+```
+
+JSONEncode
+----------
+''Example''
 ```lua
   Apollo.GetPackage("Json:Utils-1.0").tPackage:Embed(self)
    
@@ -19,11 +52,41 @@ Usage
     d = nil
   }
    
-  local exportStr = self:Encode(tNewTable)
+  local exportStr = self:JSONEncode(tNewTable)
 ```
 Results
 ```
 [{"a":1,"b":"string","c":{"a":1,"b":"string"},"d":null}]
+```
+
+JSONEncodePretty
+----------------
+''Example''
+```lua
+  Apollo.GetPackage("Json:Utils-1.0").tPackage:Embed(self)
+   
+  local tNewTable = {
+    a = 1, 
+    b = "string", 
+    c = { a = 1, b = "string" }
+    d = nil
+  }
+   
+  local exportStr = self:JSONEncodePretty(tNewTable)
+```
+Results
+```
+[
+  {
+    "a": 1,
+	"b": "string",
+	"c": {
+	  "a": 1,
+	  "b": "string"
+	},
+	"d": null
+  }
+]
 ```
 
 ----
