ee455b009ed595f37b1f07e21ef40ae0d019d7d1
Chronosis <jayreardon@gmail.com>
2015-10-07 17:39:16 -0700

    Squashed 'libs/Chronology/' changes from 62a2796..4d4e312
    4d4e312 Merge branch 'release/Chronology-1.0'
    7e58610 Change LICENSE to MIT License and update README.md
    965fbfa Add License Information
    afb9ac8 Fix Typo
    
    git-subtree-dir: libs/Chronology
    git-subtree-split: 4d4e312ce91c65b093a9c76488dc40c2a011c9a6

--- Chronology.lua
+++ Chronology.lua
@@ -1,5 +1,5 @@
 local PackageName, Major, Minor, Patch = "Chronology", 2, 0, 0
-local PkgMajor, PkgMinor = PackageName, tonumber(sting.format("%02d%02d%02d", Major, Minor, Patch))
+local PkgMajor, PkgMinor = PackageName, tonumber(string.format("%02d%02d%02d", Major, Minor, Patch))
 local Pkg = Apollo.GetPackage(PkgMajor)
 if Pkg and (Pkg.nVersion or 0) >= PkgMinor then
   return -- no upgrade needed
@@ -7,6 +7,7 @@ end
 
 -- Set a reference to the actual package or create an empty table
 local Chronology = Pkg and Pkg.tPackage or {}
+
 
 local ktMonthNumDays = {
   [1] = 31,
